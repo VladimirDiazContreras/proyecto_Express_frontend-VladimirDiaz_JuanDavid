3b98f747985f9409dbbed662fdf8f2ff717468db
unknown <diazc04080910@gmail.com>
2025-09-25 20:58:02 -0500

    feat: :sparkles: create indedx, css and js

---
+++ script3.js
@@ -0,0 +1,20 @@
+// Burger Menu
+const burger = document.getElementById("burger");
+const sideMenu = document.getElementById("sideMenu");
+
+burger.addEventListener("click", () => {
+  sideMenu.classList.toggle("active");
+});
+
+// Cerrar menú al hacer click en un enlace
+document.querySelectorAll(".side-menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    sideMenu.classList.remove("active");
+  });
+});
+
+// Cerrar sesión
+document.querySelector(".user-options .btn").addEventListener("click", () => {
+  alert("Sesión cerrada. Volviendo al login...");
+  window.location.href = "index.html";
+});
